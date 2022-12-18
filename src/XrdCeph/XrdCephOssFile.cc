b4dfaeef4795930509cf5b7c4926c23af9a02f07
Alexander Rogovskiy <alexander.rogovskiy@stfc.ac.uk>
2022-12-18 06:33:58 +0000

    Complete refactoring

--- src/XrdCeph/XrdCephOssFile.cc
+++ src/XrdCeph/XrdCephOssFile.cc
@@ -80,83 +80,105 @@ ssize_t XrdCephOssFile::ReadRaw(void *buff, off_t offset, size_t blen) {
 }
 
 ssize_t XrdCephOssFile::process_block(off_t block_start, size_t block_len, std::vector<int> chunks_to_read, XrdOucIOVec *readV) {
-  char * buf = new char[READV_BUFFER_SIZE];
-  char *ptr;
+  char *ptr, *buf;
   ssize_t data_read, real_data_read;
   data_read = 0;
-  printf("processing block %lu %lu\n", block_start, block_len);
   if (chunks_to_read.size() == 1) {
     int idx = chunks_to_read[0];
     data_read = ceph_async_read(m_fd, (void*)readV[idx].data, readV[idx].size, readV[idx].offset);
   } else {
+    try {
+      buf = new char[READV_BUFFER_SIZE];
+    } catch(std::bad_alloc&) {
+       //FIXME: use log function
+       printf("Can not allocate memory for readv buffer! Exiting\n");
+       return -ENOMEM;
+    }
+    //printf("Going to read %ld,%ld \n", block_len, block_start);
     real_data_read = ceph_async_read(m_fd, (void*) buf, block_len, block_start);
-    printf("read %ld bytes\n", real_data_read);
     if (real_data_read < (ssize_t)block_len) {
-      //log("Requested %lu bytes, got %lu!\n", block_len, data_read);
-      return -data_read;
+      //FIXME: use log function
+      printf("Expected %lu bytes, got %ld. Exiting\n", block_len, real_data_read);
+      delete[] buf;
+      return -ESPIPE;
     }
     for (int i: chunks_to_read) {
       ptr = buf;
       ptr += readV[i].offset - block_start;
+      //printf("Extracting chunk %d, ptr_pos: %lld, chunks size: %d, offset: %lld, data_read: %ld\n", i, readV[i].offset - block_start, readV[i].size, readV[i].offset, real_data_read);
       memcpy(readV[i].data, ptr, readV[i].size);
       data_read += readV[i].size;
-      printf("Extracting block %d, data read %ld\n",i,  data_read);
     }
+    delete[] buf;
   }
-  delete[] buf;
-  printf("returning  %ld\n", data_read);
   return data_read;
 }
 
 ssize_t XrdCephOssFile::ReadV(XrdOucIOVec *readV, int n) {
-  ssize_t data_read, chunks_data_read, block_start, block_len, tlen;
+  ssize_t data_read, block_data_read, block_start, block_len, cur_end, new_end;
+  ssize_t new_block_start, new_block_len;
   std::vector<int> chunks_to_read;
+  int ceph_read_count = 0;
   block_start = -1;
   block_len = 0;
   data_read = 0;
-  printf("starting readv\n");
+  //printf("Readv started\n");
   for (int i = 0; i < n; i++) {
-    printf("start: %lu, len: %lu, rstart: %lld, rlen: %d\n", block_start, block_len, readV[i].offset, readV[i].size);
-    if (block_start > 0) {
-      tlen = readV[i].offset + readV[i].size - block_start;
-      if (tlen > READV_BUFFER_SIZE || block_len > READV_BUFFER_SIZE || readV[i].offset < block_start) {
-        chunks_data_read = process_block(block_start, block_len, chunks_to_read, readV);
-        if (chunks_data_read > 0) {
-          data_read += chunks_data_read;
+    //Calculate new block borders, after a chunk will be added to the block
+    //To do so first calculate end of current block and end of chunk
+    cur_end = block_start + block_len - 1;
+    new_end = readV[i].offset + readV[i].size - 1;
+
+    //Update block start if new chunk's start is preced it.
+    if (readV[i].offset < block_start or block_start < 0) {
+      new_block_start = readV[i].offset;
+    } else {
+      new_block_start = block_start;
+    }
+
+    //Update block end if chunk's end is greater than the block's one
+    if (cur_end > new_end) {
+      new_end = cur_end;
+    }
+
+    //Now we know updated block's start and end, let's update its lengt
+    new_block_len = new_end - new_block_start + 1;
+
+    //printf("block_start: %ld, block_len: %ld, new_block_start: %ld, new_block_len: %ld , offset: %lld, size: %d, i: %d\n", block_start, block_len, new_block_start, new_block_len, readV[i].offset, readV[i].size, i);
+    if (new_block_len > READV_BUFFER_SIZE && block_len > 0) {
+        block_data_read = process_block(block_start, block_len, chunks_to_read, readV);
+        if (block_data_read > 0) {
+          data_read += block_data_read;
+          ceph_read_count++;
         } else {
-          //log("Error while reading block at %ul, got %d\n", block_start, chunks_data_read);
-          printf("Emergency exit, %ld bytes\n", chunks_data_read);
-          return chunks_data_read;
+          //FIXME: use log function
+          printf("Error while reading block at %ld, got %ld\n", block_start, block_data_read);
+          return block_data_read;
         }
-        block_start = -1;
-        block_len = 0;
-        tlen = readV[i].offset + readV[i].size - block_start;
         chunks_to_read.clear();
-      }
-      chunks_to_read.push_back(i);
-      if (block_start < 0) {
         block_start = readV[i].offset;
-      }
-      if (tlen > block_len) {
-        block_len = tlen;
-      }
+        block_len = readV[i].size;
     } else {
-      block_start = readV[i].offset;
-      block_len = readV[i].size;
-      chunks_to_read.push_back(i);
+      block_start = new_block_start;
+      block_len = new_block_len;
+    }
+    chunks_to_read.push_back(i);
+  }
+
+  //Extract chunks from the last block
+  if (chunks_to_read.size() > 0) {
+    block_data_read = process_block(block_start, block_len, chunks_to_read, readV);
+    if (block_data_read > 0) {
+      data_read += block_data_read;
+      ceph_read_count++;
+    } else {
+      //FIXME: use log function
+      printf("Error while reading block at %ld, got %ld\n", block_start, block_data_read);
+      return block_data_read;
     }
   }
-  if (block_len > 0) {
-    chunks_data_read = process_block(block_start, block_len, chunks_to_read, readV);
-    if (chunks_data_read > 0) {
-      data_read += chunks_data_read;
-    } else {
-      //log("Error while reading block at %ul, got %d\n", block_start, chunks_data_read);
-      printf("Emergency exit, %ld bytes\n", chunks_data_read);
-      return chunks_data_read;
-    }
-  }
-  printf("All read %ld bytes\n", data_read);
+  //FIXME: use log function
+  printf("%d reads issued for %d blocks\n", ceph_read_count, n);
 
   return data_read;
 }

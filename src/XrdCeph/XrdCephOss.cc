7b4aec67a9d1019f517babc62f2b48a6fb4cba1e
Ian Johnson <ijjorama@gmail.com>
2023-03-03 13:28:20 +0000

    Disk space reporting (#36)
    * Provide XrdCephOss::StatLS and ceph_posix_stat_pool to enable disk space reporting. Responds to the 'xrdfs query space' command as requested by ALICE VO
    
    * Remove ts() timestamp function and unnecessary #defines
    
    * Read ceph.poolnames setting from XRootD config to specify reportable pools.
    
    * Support 'xrdfs spaceinfo' via Stat() method returning XrdOssOK for stat'ing 'pool:'
    
    * Tidy up tracing of Stat* calls
    
    * Remove unwanted method isPathReportablePool
    
    * Add comments for need to support stat-ing '/'
    
    * Return -ENOMEM if malloc fails
    
    * Return -ENOMEM if malloc fails
    
    * Rename disk space reporting config item to ceph,reportingppols and log if the list of names is not present. Report if ceph_posix_stat_pool call to get the amount of used space fails
    
    * Sanitize incoming pool name and allow for MonALISA format
    
    * Optional tracing of Stat* incoming paths and response. Remove double logging of ceph.reporting pools.
    
    * Check that sanitized pool name is not marked invalid
    
    * Use ceph namelib translation at Oss level by copying translateFileName logic from Posix level. More error checking if stat can't find pool name.
    
    * Remove superfluous comments
    
    * Ensure tracing of path arguments to Stat() and StatLS(). Add Doxygen-style commments to changed methods
    
    * Make source tarball only as minimum output
    
    * Add make-src-tar.sh to additionally place required source tarball in '--output' destination
    
    * Change back usedSpace to totalSpace in ceph_posix_statfs

--- src/XrdCeph/XrdCephOss.cc
+++ src/XrdCeph/XrdCephOss.cc
@@ -217,7 +217,6 @@ int XrdCephOss::Configure(const char *configfn, XrdSysError &Eroute) {
          }
        }
 
-       int pread_flag_set = !strncmp(var, "ceph.usedefaultpreadalg", 24);
        int readv_flag_set = !strncmp(var, "ceph.usedefaultreadvalg", 24);
        if (pread_flag_set or readv_flag_set) {
          var = Config.GetWord();
@@ -403,7 +402,6 @@ int XrdCephOss::Rename(const char *from,
 
 /**
  *
-
  * @brief Extract a pool name (string before the first colon ':') from an object ID.
  * @param (in) possPool the object ID
  * @return pool name or unchanged object ID

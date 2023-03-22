ef23f27b98384b4b6a9a12badb71f704bd37ebf0
Alexander Rogovskiy <alexander.rogovskiy@stfc.ac.uk>
2023-03-22 17:45:08 +0000

    feat: refactor BulkAioRead::read method, suggested during review
    1. Rename end_block to last_block
    2. Move variable definitions closer to its usage
    3. Use 'std::min' instead of 'if' for chunk_len determination
    4. Use more efficient chunk_start calculation

--- src/XrdCeph/XrdCephBulkAioRead.cc
+++ src/XrdCeph/XrdCephBulkAioRead.cc
@@ -113,30 +113,36 @@ ssize_t bulkAioRead::get_results() {
 
 int bulkAioRead::read(void* out_buf, size_t req_size, off64_t offset) {
 
-  size_t start_block, end_block, buf_pos, chunk_len, chunk_start, req_len;
+  size_t start_block, last_block, buf_pos, chunk_len, chunk_start, req_len;
   char *buf_ptr;
-  //16 bytes for object hex number, 1 for dot and 1 for null-terminator
-  char object_suffix[18];
-  std::string obj_name;
-  int rc;
+
+  if (req_size == 0) {
+    return -EINVAL;
+  }
 
   req_len = req_size;
   start_block = offset / block_size;
-  end_block = (offset + req_len - 1) / block_size;
+  last_block = (offset + req_len - 1) / block_size;
   buf_ptr = (char*) out_buf;
   buf_pos = 0;
+  chunk_start = offset % block_size;
 
-  while (start_block <= end_block) {
-    sprintf(object_suffix, ".%016zx", start_block);
+  while (start_block <= last_block) {
+    //16 bytes for object hex number, 1 for dot and 1 for null-terminator
+    char object_suffix[18];
+    int sp_bytes_written;
+    sp_bytes_written = snprintf(object_suffix, sizeof(object_suffix), ".%016zx", start_block);
+    if (sp_bytes_written > (int) sizeof(object_suffix)) {
+      log_func((char*)"Can not fit object suffix into buffer for file %s\n", file_name.c_str());
+      return -EFBIG;
+    } 
+
+    std::string obj_name;
     obj_name =  file_name + std::string(object_suffix);
 
-    chunk_start = offset % block_size;
-    if (req_len < block_size - chunk_start) {
-      chunk_len = req_len;
-    } else {
-      chunk_len = block_size - chunk_start;
-    }
+    chunk_len = std::min(req_len, block_size - chunk_start);
 
+    int rc;
     rc = addRequest(obj_name, buf_ptr, chunk_len, chunk_start);
     if (rc < 0) {
       log_func((char*)"Unable to submit async read request, rc=%d\n", rc);
@@ -145,12 +151,12 @@ int bulkAioRead::read(void* out_buf, size_t req_size, off64_t offset) {
     buf_pos += chunk_len;
     if (buf_pos > req_size) {
       log_func((char*)"Internal bug! Attempt to read %lu data for block (%lu, %lu) of object %s\n", buf_pos, offset, req_size, obj_name.c_str());
-      return rc;
+      return -EINVAL;
     }
     buf_ptr += chunk_len;
 
     start_block++;
-    offset = start_block*block_size;
+    chunk_start = 0;
     req_len = req_len - chunk_len;
   }
   return 0;   

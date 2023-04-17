edf310f18222feacebc3e879754cc96b3ee8781f
root <root@ceph1.etc.grid.kiae.ru>
2023-04-17 16:20:07 +0000

    fix: remove move constructor from bulkAioRead
    We do not use it.

--- src/XrdCeph/XrdCephBulkAioRead.hh
+++ src/XrdCeph/XrdCephBulkAioRead.hh
@@ -24,7 +24,6 @@ class bulkAioRead {
    */ 
   public:
   bulkAioRead(librados::IoCtx *ct, logfunc_pointer ptr, std::string filename, size_t object_size);
-  bulkAioRead(bulkAioRead&& tmp);
   ~bulkAioRead();
 
   void clear();
